c350607b44fc7c9a85a051b9d99f61516d0ab3ae
jesufran <franklin.porras.mejia@gmail.com>
2025-09-07 06:14:12 -0600

    add firebase config

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+// TODO: Reemplaza esto con la configuración de tu propio proyecto de Firebase.
+// Ve a: Project settings > General > Your apps > SDK setup and configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvakPCZmNW1ATbCPopImzlwn-G-t_Y0AU",
+  authDomain: "gestorarchivosia.firebaseapp.com",
+  projectId: "gestorarchivosia",
+  storageBucket: "gestorarchivosia.firebasestorage.app",
+  messagingSenderId: "861117370758",
+  appId: "1:861117370758:web:fb90075281b6f6c18a5c07"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
+export { storage };
